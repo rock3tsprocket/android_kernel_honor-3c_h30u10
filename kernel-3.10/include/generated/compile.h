f4b08ddd2f226332500fcd424a93cb55446fa15d
Manjot Sidhu <manjot.gni@gmail.com>
2017-07-15 16:46:19 +0530

    Build Successful

---
+++ kernel-3.10/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Sat Jul 15 16:30:07 IST 2017"
+#define LINUX_COMPILE_BY "manjotsidhu"
+#define LINUX_COMPILE_HOST "android-unite2"
+#define LINUX_COMPILER "gcc version 4.8 (GCC) "
